7ec79270579b96d7cad785b0b5545bea9a54c5f9
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-24 16:39:07 +0300

    Update switch.c

--- switch.c
+++ switch.c
@@ -1,1 +1,28 @@
+#include "main.h"
 
+int print_argument(const char *format, va_list args, int count_of_printed) {
+    switch (*format) {
+        case 'd':
+        case 'i':
+            count_of_printed = print_integer(va_arg(args, int), count_of_printed);
+            break;
+        case 'c':
+            putchar(va_arg(args, int));
+            count_of_printed++;
+            break;
+        case 's':
+            count_of_printed = print_string(va_arg(args, char *), count_of_printed);
+            break;
+        case '%':
+            putchar('%');
+            count_of_printed++;
+            break;
+        case 'b':
+            count_of_printed = print_binary(va_arg(args, unsigned int), count_of_printed);
+            break;
+        default:
+            // Unknown conversion specifier - ignore it
+            break;
+    }
+    return count_of_printed;
+}
