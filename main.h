81a114948ff90954ebe00f9072a737259a931db3
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-24 16:13:30 +0300

    Update main.h function prototypes

--- main.h
+++ main.h
@@ -5,9 +5,9 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-int print_char(va_list args, int count);
-int print_string(va_list args, int count);
-int print_integer(va_list args, int count);
+int print_char(va_list args, int count_of_printed);
+int print_string(va_list args, int count_of_printed);
+int print_integer(va_list args, int count_of_printed);
 int _printf(const char *format, ...);
 
 #endif
