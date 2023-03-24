c27fd9abe0b96d52efc69c6e239a91e50df06139
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-24 16:41:34 +0300

    Update main.c

--- main.c
+++ main.c
@@ -8,6 +8,6 @@ int main() {
     _printf("%%\n");         // Output: %
     _printf("c = %c\n", 'v');  // Output: c = x
     _printf("i = %i\n", 999);  // Output: i = -123
-    _printf("My name is %s and I'm writing this project with %s who is %d years old and has %i cars and nickname %%", "Antony", "Oyengo", 25, 28, "%");  // Output: i = -123
+    _printf("Binary symbol number: %b My name is %s and I'm writing this project with %s who is %d years old and has %i cars and nickname %%", 44, "Antony", "Oyengo", 25, 28, "%");  // Output: i = -123
     return 0;
 }
