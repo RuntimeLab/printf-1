b5aa086724ff408bd97a6209de628d2a0ba23178
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-24 11:22:52 +0300

    Update printc.c

--- printc.c
+++ printc.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 
 /**
- * printc - prints a char.
+ * printf_c - prints a char.
  * @pa: format of char.
  * @format: format
  * Return: number char for printf.
  */
-int printc(char *format, va_list pa)
+int printf_c(char *format, va_list pa)
 {
 	(void)format;
 	_putchar(va_arg(pa, int));
