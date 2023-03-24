8f6b5cda8666f92e1b1a2eedf05293895fb60140
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-24 17:03:51 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,61 +1,23 @@
 #include "main.h"
 
-
-int _printf(const char *format, ...)
-{
+int _printf(const char *format, ...) {
     va_list args;
-    int count_of_printed = 0;
-
     va_start(args, format);
+
+    int count_of_printed = 0;
 
     while (*format != '\0') {
         if (*format == '%') {
             format++;
-
-            // Handle escape sequence %%
-            if (*format == '%') {
-                putchar('%');
-                count_of_printed++;
-                format++;
-                continue;
-            }
-
-            // Handle character conversion specifier %c
-            if (*format == 'c') {
-                count_of_printed = print_char(args, count_of_printed);
-                format++;
-                continue;
-            }
-
-            // Handle string conversion specifier %s
-            if (*format == 's') {
-                count_of_printed = print_string(args, count_of_printed);
-                format++;
-                continue;
-            }
-
-            // Handle integer conversion specifiers %d and %i
-            if (*format == 'd' || *format == 'i') {
-                count_of_printed = print_integer(args, count_of_printed);
-                format++;
-                continue;
-            }
-            
-            // Handle integer conversion specifiers %b
-            if (*format == 'b') {
-                count_of_printed = print_binary(va_arg(args, unsigned int), count_of_printed);
-                format++;
-                continue;
-            }
+            count_of_printed = print_argument(format, args, count_of_printed);
+            format++;
+        } else {
+            putchar(*format);
+            count_of_printed++;
+            format++;
         }
-
-        // Handle regular characters
-        putchar(*format);
-        count_of_printed++;
-        format++;
     }
 
     va_end(args);
-
-    return count_of_printed;
+    return count;
 }
