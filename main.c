5a377599834199e405da2d85e4020e38876c5727
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-24 17:14:12 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,11 +3,11 @@
 int main() {
     int number = 42;
     char *string = "Hello my name is Antony Oyengo!";
-    _printf("a = %d\n", number);  // Output: a = 42
-    _printf("s = %s\n", string);  // Output: s = Hello, world!
-    _printf("%%\n");         // Output: %
-    _printf("c = %c\n", 'v');  // Output: c = x
-    _printf("i = %i\n", 999);  // Output: i = -123
+    _printf("This is a number: = %d\n", number);  // Output: a = 42
+    _printf("This is a string: = %s\n", string);  // Output: s = Hello, world!
+    _printf("This is a percentage sigh: %%\n");         // Output: %
+    _printf("This is a character: %c\n", 'v');  // Output: c = x
+    _printf("This is another number: %i\n", 999);  // Output: i = -123
     _printf("Binary symbol number: %b My name is %s and I'm writing this project with %s who is %d years old and has %i cars and nickname %%", 44, "Antony", "Oyengo", 25, 28, "%");  // Output: i = -123
     return 0;
 }
