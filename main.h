cb54fcf8d86dd4958bdf34d51d2196b9cda35b3e
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-24 17:08:40 +0300

    Update main.h

--- main.h
+++ main.h
@@ -8,7 +8,7 @@
 int print_char(va_list args, int count_of_printed);
 int print_string(va_list args, int count_of_printed);
 int print_integer(va_list args, int count_of_printed);
-int print_argument(const char *format, va_list args, int count_of_printed);
+int select_specifier(const char *format, va_list args, int count_of_printed);
 int print_binary(unsigned int num, int count_of_printed);
 int _printf(const char *format, ...);
 
