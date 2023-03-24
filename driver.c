45c2d242268a2c238c44fba8778b064a99dc4a73
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-24 11:31:20 +0300

    Update driver.c

--- driver.c
+++ driver.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * driver - selector for type of fun.
@@ -14,14 +14,14 @@ int (*driver(char *format))(char *format, va_list)
 int i;
 
 structype selector[] = {
-{"%c", printc},
-{"%s", printstr},
-{"%d", printint},
-{"%i", printint},
-{"%%", printpercent},
-{"%x", printhex},
-{"%X", printHEX},
-{"%o", printocta},
+{"%c", printf_c},
+{"%s", printf_s},
+{"%d", printf_d},
+{"%i", printf_d},
+{"%%", printf_percent},
+{"%x", printf_x},
+{"%X", printf_X},
+{"%o", printf_o},
 {NULL, NULL}
 };
 
