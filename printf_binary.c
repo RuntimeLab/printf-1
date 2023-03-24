95da4e949abb86a1129e06ef17e2a69226b21562
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-24 16:30:22 +0300

    Create printf_binary.c

---
+++ printf_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+int print_binary(unsigned int num, int count_of_printed) {
+    if (num == 0) {
+        putchar('0');
+        count_of_printed++;
+        return count_of_printed;
+    }
+
+    int binary[32] = {0};
+    int i = 0;
+
+    // Convert the number to binary
+    while (num > 0) {
+        binary[i] = num % 2;
+        num /= 2;
+        i++;
+    }
+
+    // Print the binary digits from right to left
+    while (i > 0) {
+        i--;
+        putchar('0' + binary[i]);
+        count_of_printed++;
+    }
+
+    return count_of_printed;
+}
