d48c210220f80385ab2c7bf0ee9fa2f2cf067bd5
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-24 16:54:33 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,30 +1,58 @@
-#include "main.h"
-
-int _printf(const char *format, ...) {
+int _printf(const char *format, ...)
+{
     va_list args;
-    va_start(args, format);
+    int count = 0;
 
-    int count_of_printed = 0;
+    va_start(args, format);
 
     while (*format != '\0') {
         if (*format == '%') {
             format++;
-            count_of_printed = print_argument(format, args, count_of_printed);
-            format++;
-        } else {
-            putchar(*format);
-            count_of_printed++;
-            format++;
+
+            // Handle escape sequence %%
+            if (*format == '%') {
+                putchar('%');
+                count++;
+                format++;
+                continue;
+            }
+
+            // Handle character conversion specifier %c
+            if (*format == 'c') {
+                count = print_char(args, count);
+                format++;
+                continue;
+            }
+
+            // Handle string conversion specifier %s
+            if (*format == 's') {
+                count = print_string(args, count);
+                format++;
+                continue;
+            }
+
+            // Handle integer conversion specifiers %d and %i
+            if (*format == 'd' || *format == 'i') {
+                count = print_integer(args, count);
+                format++;
+                continue;
+            }
+            
+            // Handle integer conversion specifiers %b
+            if (*format == 'b') {
+                count = print_binary(args, count);
+                format++;
+                continue;
+            }
         }
+
+        // Handle regular characters
+        putchar(*format);
+        count++;
+        format++;
     }
 
     va_end(args);
-    return count_of_printed;
+
+    return count;
 }
-
-
-
-
-
-
-
