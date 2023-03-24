5e244ee19e47caff7473d326d035c00e0467f2d8
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-24 18:54:11 +0300

    Update main.c

--- main.c
+++ main.c
@@ -14,6 +14,7 @@ int main() {
     _printf("This is your hex: %x\n", my_hex);
     _printf("This is your octal: %o\n", my_octal);
     _printf("This is your unsigned number: %u\n", my_unsigned);
+    _printf("Hello the Oyengo. Or should I say %r!\n", "Oyengo");
     _printf("Binary symbol number: %b My name is %s and I'm writing this project with %s who is %d years old and has %i cars and nickname %%\n", 44, "Antony", "Oyengo", 25, 28, "%");
     return 0;
 }
