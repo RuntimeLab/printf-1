9ded3838f38647f25b753e35f07174d401b9d972
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-24 18:12:11 +0300

    Update switch.c

--- switch.c
+++ switch.c
@@ -4,31 +4,31 @@ int select_specifier(const char *format, va_list args, int count_of_printed) {
     switch (*format) {
         case 'd':
         case 'i':
-            count_of_printed = print_integer(args, count_of_printed);
+            count_of_printed = printf_integer(args, count_of_printed);
             break;
         case 'c':
             putchar(va_arg(args, int));
             count_of_printed++;
             break;
         case 's':
-            count_of_printed = print_string(args, count_of_printed);
+            count_of_printed = printf_string(args, count_of_printed);
             break;
         case '%':
             putchar('%');
             count_of_printed++;
             break;
         case 'b':
-            count_of_printed = print_binary(va_arg(args, unsigned int), count_of_printed);
+            count_of_printed = printf_binary(va_arg(args, unsigned int), count_of_printed);
             break;
         case 'x':
         case 'X':
-            count_of_printed = print_hex(va_arg(args, unsigned int), count_of_printed, (*format == 'X') ? 1 : 0);
+            count_of_printed = printf_hex(va_arg(args, unsigned int), count_of_printed, (*format == 'X') ? 1 : 0);
             break;
         case 'o':
-            count_of_printed = print_octal(va_arg(args, unsigned int), count_of_printed);
+            count_of_printed = printf_octal(va_arg(args, unsigned int), count_of_printed);
             break;
         case 'u':
-            count_of_printed = print_unsigned(va_arg(args, unsigned int), count_of_printed);
+            count_of_printed = printf_unsigned(va_arg(args, unsigned int), count_of_printed);
             break;
         default:
             // Unknown conversion specifier - ignore it
