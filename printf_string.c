621f424405beffd596ad6fc661d9c51ad3ac312d
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-24 18:42:47 +0300

    Update printf_string.c

--- printf_string.c
+++ printf_string.c
@@ -1,24 +1,12 @@
 #include "main.h"
 
-int printf_string(va_list args, int count_of_printed) {
-    char* str = va_arg(args, char*);
-    int i = 0;
-    while (str[i]) {
-        if ((str[i] > 0 && str[i] < 32) || str[i] >= 127) {
-            char hex[3];
-            hex[0] = '\\';
-            hex[1] = 'x';
-            int val = str[i];
-            hex[2] = (val / 16) < 10 ? (val / 16) + '0' : (val / 16) - 10 + 'A';
-            hex[3] = (val % 16) < 10 ? (val % 16) + '0' : (val % 16) - 10 + 'A';
-            write(1, hex, 4);
-            count_of_printed += 4;
-        }
-        else {
-            write(1, &str[i], 1);
-            count_of_printed++;
-        }
-        i++;
+int printf_string(va_list args, int count_of_printed)
+{
+    char *string = va_arg(args, char *);
+    while (*string != '\0') {
+        putchar(*string);
+        count_of_printed++;
+        string++;
     }
     return count_of_printed;
 }
