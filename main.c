8d3e96c8e3b1c3c60b822c404cba4185cc8ad8fd
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-24 18:18:40 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,11 +3,17 @@
 int main() {
     int number = 42;
     char *string = "Hello my name is Antony Oyengo!";
-    _printf("This is a number: = %d\n", number);  // Output: a = 42
-    _printf("This is a string: = %s\n", string);  // Output: s = Hello, world!
-    _printf("This is a percentage sigh: %%\n");         // Output: %
-    _printf("This is a character: %c\n", 'v');  // Output: c = x
-    _printf("This is another number: %i\n", 999);  // Output: i = -123
+    unsigned int my_hex = 0x126AFC;
+    unsigned int my_unsigned = 1234567890;
+    unsigned int my_octal = 01234567;  
+    _printf("This is a number: = %d\n", number); 
+    _printf("This is a string: = %s\n", string);
+    _printf("This is a percentage sigh: %%\n");
+    _printf("This is a character: %c\n", 'v');
+    _printf("This is another number: %i\n", 999);
+    _printf("This is your hex: %x", my_hex);
+    _printf("This is your octal: %o", my_octal);
+    _printf("This is your unsigned number: %u", my_unsigned);
     _printf("Binary symbol number: %b My name is %s and I'm writing this project with %s who is %d years old and has %i cars and nickname %%", 44, "Antony", "Oyengo", 25, 28, "%");  // Output: i = -123
     return 0;
 }
