874e21e321266c9667068c5be630ff5d1380e54b
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-26 09:37:41 +0300

    Update printf_pointer.c

--- printf_pointer.c
+++ printf_pointer.c
@@ -6,31 +6,26 @@ int printf_pointer(va_list args, int printed) {
     int digits = 0;
     unsigned long long temp = num;
 
-    // Count the number of digits in the hexadecimal representation of the pointer
     while (temp != 0) {
         digits++;
         temp /= 16;
     }
-
-    // Print the '0x' prefix
+    
     printed += _putchar('0');
     printed += _putchar('x');
 
-    // Print the hexadecimal representation of the pointer
     if (num == 0) {
         printed += _putchar('0');
     } else {
         char hex_digits[16] = "0123456789abcdef";
         char hex[digits];
 
-        // Convert the pointer value to hexadecimal
         for (int i = digits - 1; i >= 0; i--) {
             int digit = num % 16;
             hex[i] = hex_digits[digit];
             num /= 16;
         }
 
-        // Print the hexadecimal digits
         for (int i = 0; i < digits; i++) {
             printed += _putchar(hex[i]);
         }
