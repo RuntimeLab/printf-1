46d9d5fc4305fddede218be846a02e6d4b440506
antonymuga <anthony.ochieng.muga@gmail.com>
2023-03-25 22:28:15 -0700

    Made _printf betty compliant

--- _printf.c
+++ _printf.c
@@ -1,23 +1,33 @@
 #include "main.h"
 
-int _printf(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
+/**
+ * _printf - implementation of the inbuilt printf
+ * @format: the format specifier
+ * Return: the formated string
+ */
 
-    int count_of_printed = 0;
+int _printf(const char *format, ...)
+{
+	va_list args;
 
-    while (*format != '\0') {
-        if (*format == '%') {
-            format++;
-            count_of_printed = select_specifier(format, args, count_of_printed);
-            format++;
-        } else {
-            putchar(*format);
-            count_of_printed++;
-            format++;
-        }
-    }
+	va_start(args, format);
+	int count_of_printed = 0;
 
-    va_end(args);
-    return count_of_printed;
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			count_of_printed = select_specifier(format, args, count_of_printed);
+			format++;
+		}
+		else
+		{
+			putchar(*format);
+			count_of_printed++;
+			format++;
+		}
+	}
+	va_end(args);
+	return (count_of_printed);
 }
--- switch.c
+++ switch.c
@@ -1,44 +1,45 @@
 #include "main.h"
 
-int select_specifier(const char *format, va_list args, int count_of_printed) {
-    switch (*format) {
-        case 'd':
-        case 'i':
-            count_of_printed = printf_integer(args, count_of_printed);
-            break;
-        case 'c':
-            putchar(va_arg(args, int));
-            count_of_printed++;
-            break;
-        case 's':
-            count_of_printed = printf_string(args, count_of_printed);
-            break;
-        case '%':
-            putchar('%');
-            count_of_printed++;
-            break;
-        case 'b':
-            count_of_printed = printf_binary(va_arg(args, unsigned int), count_of_printed);
-            break;
-        case 'x':
-        case 'X':
-            count_of_printed = printf_hex(va_arg(args, unsigned int), count_of_printed, (*format == 'X') ? 1 : 0);
-            break;
-        case 'o':
-            count_of_printed = printf_octal(va_arg(args, unsigned int), count_of_printed);
-            break;
-        case 'u':
-            count_of_printed = printf_unsigned(va_arg(args, unsigned int), count_of_printed);
-            break;
-        case 'r':
-            count_of_printed = printf_reverse(args, count_of_printed);
-            break;
-        case 'p':
-            count_of_printed = printf_pointer(args, count_of_printed);
-            break;
-        default:
-            // Unknown conversion specifier - ignore it
-            break;
-    }
-    return count_of_printed;
+int select_specifier(const char *format, va_list args, int count_of_printed)
+{
+	switch (*format)
+	{
+		case 'd':
+		case 'i':
+			count_of_printed = printf_integer(args, count_of_printed);
+			break;
+		case 'c':
+			putchar(va_arg(args, int));
+			count_of_printed++;
+			break;
+		case 's':
+			count_of_printed = printf_string(args, count_of_printed);
+			break;
+		case '%':
+			putchar('%');
+			count_of_printed++;
+			break;
+		case 'b':
+			count_of_printed = printf_binary(va_arg(args, unsigned int), count_of_printed);
+			break;
+		case 'x':
+		case 'X':
+			count_of_printed = printf_hex(va_arg(args, unsigned int), count_of_printed, (*format == 'X') ? 1 : 0);
+			break;
+		case 'o':
+			count_of_printed = printf_octal(va_arg(args, unsigned int), count_of_printed);
+			break;
+		case 'u':
+			count_of_printed = printf_unsigned(va_arg(args, unsigned int), count_of_printed);
+			break;
+		case 'r':
+			count_of_printed = printf_reverse(args, count_of_printed);
+			break;
+		case 'p':
+			count_of_printed = printf_pointer(args, count_of_printed);
+			break;
+		default:
+			break;
+	}
+	return count_of_printed;
 }
