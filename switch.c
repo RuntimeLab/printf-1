8c79ad90c12895eebba24e7e0767693a5bef942b
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-26 12:53:40 +0300

    Update switch.c

--- switch.c
+++ switch.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * selector - selects the appropriate specifiers
+ * @args: number of arguements
+ * @printed: the printed characters
+ * @format: the format specifier
+ * Return: printed charcaters
+ */
+
 int selector(const char *format, va_list args, int printed)
 {
 	switch (*format)
