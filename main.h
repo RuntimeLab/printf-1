3dcd61b755436c9dedb10e8a8064f8cfa579d362
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-26 08:47:47 +0300

    Update main.h

--- main.h
+++ main.h
@@ -5,6 +5,7 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+int _putchar(char character);
 int printf_char(va_list args, int printed);
 int printf_string(va_list args, int printed);
 int printf_integer(va_list args, int printed);
