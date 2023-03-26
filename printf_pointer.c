7929618dba30919725afe1caebe618657c1e00e9
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-26 12:55:10 +0300

    Update printf_pointer.c

--- printf_pointer.c
+++ printf_pointer.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * printf_binary - prints a binary number
+ * printf_pointer - prints a binary number
  * @args: numberof arguements
  * @printed: the printed characters
  * Return: printed charcaters
