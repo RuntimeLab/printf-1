9c6c98461a16b1956e6394ae4dfcd17df1df0514
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-26 12:48:09 +0300

    Update printf_char.c

--- printf_char.c
+++ printf_char.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * printf_binary - prints a binary number
+ * @printed: the printed characters
+ * Return: printed charcaters
+ */
+
 int printf_char(va_list args, int printed)
 {
 	int character = va_arg(args, int);
