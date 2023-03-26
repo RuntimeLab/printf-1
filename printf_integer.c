359808be2811f044f6798b992632b3e26a3afc64
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-26 09:18:09 +0300

    Update printf_integer.c

--- printf_integer.c
+++ printf_integer.c
@@ -3,23 +3,31 @@
 int printf_integer(va_list args, int printed)
 {
     int num = va_arg(args, int);
+    int digits = 0;
+
+    // handle negative numbers
     if (num < 0) {
         _putchar('-');
         printed++;
         num = -num;
     }
-    int digits = 0;
+
+    // count number of digits
     int temp = num;
-    while (temp != 0) {
+    do {
         digits++;
         temp /= 10;
-    }
+    } while (temp != 0);
+
+    // print digits in correct order
     temp = num;
-    while (temp != 0) {
-        int digit = temp % 10;
-        _putchar(digit + '0');
+    for (int i = digits - 1; i >= 0; i--) {
+        int divisor = pow(10, i);
+        int digit = temp / divisor;
+        _putchar('0' + digit);
         printed++;
-        temp /= 10;
+        temp %= divisor;
     }
-    return (printed);
+
+    return printed;
 }
