1b91453af5fba48c3feac8373eefd7e03208c8d5
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-26 10:02:24 +0300

    Update printf_pointer.c

--- printf_pointer.c
+++ printf_pointer.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int print_pointer(va_list args, int printed) {
+int printf_pointer(va_list args, int printed) {
     void *ptr = va_arg(args, void*);
     unsigned long long num = (unsigned long long) ptr;
     int digits = 0;
