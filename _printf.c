af4b2689779da776ab13746d930937cbe950de01
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-26 08:30:34 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -11,23 +11,23 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
-	int count_of_printed = 0;
+	int printed = 0;
 
 	while (*format != '\0')
 	{
 		if (*format == '%')
 		{
 			format++;
-			count_of_printed = select_specifier(format, args, count_of_printed);
+			printed = selector(format, args, printed);
 			format++;
 		}
 		else
 		{
 			putchar(*format);
-			count_of_printed++;
+			printed++;
 			format++;
 		}
 	}
 	va_end(args);
-	return (count_of_printed);
+	return (printed);
 }
