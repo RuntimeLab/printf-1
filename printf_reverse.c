2726a604d58ce08bbc2591dbedece0e90eb2d9e6
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-26 12:54:49 +0300

    Update printf_reverse.c

--- printf_reverse.c
+++ printf_reverse.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * printf_binary - prints a binary number
+ * printf_reverse - prints a binary number
  * @args: number of arguements
  * @printed: the printed characters
  * Return: printed charcaters
