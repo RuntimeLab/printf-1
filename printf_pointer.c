7ddbceb6ebaf3ae88ffacd6fa9626faa75b1784f
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-26 10:05:54 +0300

    Update printf_pointer.c

--- printf_pointer.c
+++ printf_pointer.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
+#define MAX_HEX_DIGITS 16
+
 int printf_pointer(va_list args, int printed) {
     void *ptr = va_arg(args, void*);
     unsigned long num = (unsigned long) ptr;
     int digits = 0;
     int i;
     unsigned long temp = num;
-    
+    char hex_digits[MAX_HEX_DIGITS] = "0123456789abcdef";
+    char hex[MAX_HEX_DIGITS];
+
     while (temp != 0) {
         digits++;
         temp /= 16;
@@ -18,9 +22,6 @@ int printf_pointer(va_list args, int printed) {
     if (num == 0) {
         printed += _putchar('0');
     } else {
-        char hex_digits[16] = "0123456789abcdef";
-        char hex[digits];
-
         for (i = digits - 1; i >= 0; i--) {
             int digit = num % 16;
             hex[i] = hex_digits[digit];
@@ -34,4 +35,3 @@ int printf_pointer(va_list args, int printed) {
 
     return printed;
 }
-
