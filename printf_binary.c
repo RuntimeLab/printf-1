8e4ba4d9fa11129b0149a9773f7bbd6cf3f860a6
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-26 09:36:39 +0300

    Update printf_binary.c

--- printf_binary.c
+++ printf_binary.c
@@ -9,15 +9,13 @@ int printf_binary(unsigned int num, int printed) {
 
     int binary[32] = {0};
     int i = 0;
-
-    // Convert the number to binary
+    
     while (num > 0) {
         binary[i] = num % 2;
         num /= 2;
         i++;
     }
 
-    // Print the binary digits from right to left
     while (i > 0) {
         i--;
         _putchar('0' + binary[i]);
