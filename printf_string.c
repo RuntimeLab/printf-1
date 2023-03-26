3541bc27b14ca15a422db2cc1599957919601628
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-26 08:41:08 +0300

    Update printf_string.c

--- printf_string.c
+++ printf_string.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
-int printf_string(va_list args, int count_of_printed)
+int printf_string(va_list args, int printed)
 {
     char *string = va_arg(args, char *);
     while (*string != '\0') {
         putchar(*string);
-        count_of_printed++;
+        printed++;
         string++;
     }
-    return count_of_printed;
+    return printed;
 }
 
