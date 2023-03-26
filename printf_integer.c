afd543c27982379510a980e0364941197ab9e5fd
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-26 09:10:21 +0300

    Update printf_integer.c

--- printf_integer.c
+++ printf_integer.c
@@ -1,43 +1,29 @@
 #include "main.h"
 
-int printf_integer(va_list args, int printed)
-{
+int printf_integer(va_list args, int printed) {
     int num = va_arg(args, int);
     int digits = 0;
     int temp = num;
 
     if (num < 0) {
         _putchar('-');
-        printed++;
-        //temp = -temp;
-        return printed;
+        count++;
+        temp = -temp;
     }
 
-    if (temp == 0) {
-        _putchar('0');
+    do {
+        _putchar(temp % 10 + '0');
         printed++;
-        return printed;
-    }
-
-    // Determine the number of digits in the number
-    while (temp != 0) {
         digits++;
         temp /= 10;
-    }
+    } while (temp > 0);
 
-    // Print the digits of the number from left to right
-    temp = num;
-    while (digits > 0) {
-        int divisor = 1;
-        for (int i = 1; i < digits; i++) {
-            divisor *= 10;
-        }
-        int digit = temp / divisor;
-        temp %= divisor;
-        _putchar('0' + digit);
-        printed++;
-        digits--;
+    for (int i = 0; i < digits / 2; i++) {
+        int tmp = ((digits - i) > 1) ? i : digits - 1;
+        char c = ((digits - tmp) > 1) ? '0' : '-';
+        _putchar(c);
     }
 
     return (printed);
 }
+
