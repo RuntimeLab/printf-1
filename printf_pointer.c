4cd97cbd5003d5ddbbc5aa2e2ac2c84d1bb3b910
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-26 09:56:24 +0300

    Update printf_pointer.c

--- printf_pointer.c
+++ printf_pointer.c
@@ -1,36 +1,32 @@
 #include "main.h"
 
-int printf_pointer(va_list args, int printed) {
-    void *ptr = va_arg(args, void*);
-    unsigned long num = (unsigned long) ptr;
-    int digits = 0;
-    unsigned long temp = num;
-    int i;
+int print_pointer(va_list args, int printed) {
+    void *ptr = va_arg(args, void *);
+    char hex[16];
+    int i, j;
+    unsigned long int address = (unsigned long int)ptr;
+    printed += print_string("0x");
 
-    while (temp != 0) {
-        digits++;
-        temp /= 16;
+    if (address == 0) {
+        printed += print_char('0');
+        return printed;
     }
-    
-    printed += _putchar('0');
-    printed += _putchar('x');
 
-    if (num == 0) {
-        printed += _putchar('0');
-    } else {
-        char hex_digits[16] = "0123456789abcdef";
-        char hex[digits];
-
-        for (i = digits - 1; i >= 0; i--) {
-            int digit = num % 16;
-            hex[i] = hex_digits[digit];
-            num /= 16;
+    for (i = 0; address != 0; i++) {
+        int remainder = address % 16;
+        if (remainder < 10) {
+            hex[i] = remainder + '0';
+        } else {
+            hex[i] = remainder - 10 + 'a';
         }
+        address /= 16;
+    }
 
-        for (i = 0; i < digits; i++) {
-            printed += _putchar(hex[i]);
-        }
+    for (j = i - 1; j >= 0; j--) {
+        printed += print_char(hex[j]);
     }
 
     return printed;
+}
+
 }
