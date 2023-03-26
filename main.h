484921adbeb5cc7abb6148fc41ecf2228ba45be1
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-26 08:31:34 +0300

    Update main.h

--- main.h
+++ main.h
@@ -5,16 +5,16 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-int printf_char(va_list args, int count_of_printed);
-int printf_string(va_list args, int count_of_printed);
-int printf_integer(va_list args, int count_of_printed);
-int select_specifier(const char *format, va_list args, int count_of_printed);
-int printf_binary(unsigned int num, int count_of_printed);
+int printf_char(va_list args, int printed);
+int printf_string(va_list args, int printed);
+int printf_integer(va_list args, int printed);
+int select_specifier(const char *format, va_list args, int printed);
+int printf_binary(unsigned int num, int printed);
 int _printf(const char *format, ...);
-int printf_hex(unsigned int num, int count_of_printed, int uppercase);
-int printf_octal(unsigned int num, int count_of_printed);
-int printf_unsigned(unsigned int num, int count_of_printed);
-int printf_reverse(va_list args, int count_of_printed);
-int printf_pointer(va_list args, int count_of_printed);
+int printf_hex(unsigned int num, int printed, int uppercase);
+int printf_octal(unsigned int num, int printed);
+int printf_unsigned(unsigned int num, int printed);
+int printf_reverse(va_list args, int printed);
+int printf_pointer(va_list args, int printed);
 
 #endif
