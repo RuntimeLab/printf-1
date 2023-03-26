3af3ae69deef29177dc039af2e3b1411eef0e49e
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-26 09:15:06 +0300

    Update printf_integer.c

--- printf_integer.c
+++ printf_integer.c
@@ -1,29 +1,25 @@
 #include "main.h"
 
-int printf_integer(va_list args, int printed) {
+int printf_integer(va_list args, int printed)
+{
     int num = va_arg(args, int);
-    int digits = 0;
-    int temp = num;
-
     if (num < 0) {
         _putchar('-');
         printed++;
-        temp = -temp;
+        num = -num;
     }
-
-    do {
-        _putchar(temp % 10 + '0');
-        printed++;
+    int digits = 0;
+    int temp = num;
+    while (temp != 0) {
         digits++;
         temp /= 10;
-    } while (temp > 0);
-
-    for (int i = 0; i < digits / 2; i++) {
-        int tmp = ((digits - i) > 1) ? i : digits - 1;
-        char c = ((digits - tmp) > 1) ? '0' : '-';
-        _putchar(c);
     }
-
+    temp = num;
+    while (temp != 0) {
+        int digit = temp % 10;
+        _putchar(digit + '0');
+        printed++;
+        temp /= 10;
+    }
     return (printed);
 }
-
