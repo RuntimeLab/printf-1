246d1487f9f3abcb0c5a998fc1dc81724edeea82
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-03-26 08:32:30 +0300

    Update printf_char.c

--- printf_char.c
+++ printf_char.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
-int printf_char(va_list args, int count_of_printed)
+int printf_char(va_list args, int printed)
 {
     int character = va_arg(args, int);
     putchar(character);
-    return count_of_printed + 1;
+    return printed + 1;
 }
